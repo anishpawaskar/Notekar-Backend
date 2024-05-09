e24f1877b40330aa7ef2509e880cbdb96ca079b5
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-10 01:13:33 +0530

    Create routes for user authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import notesRoute from "./notes.js";
+import authRoute from "./auth.js";
 
 const app = express.Router();
 
 app.use("/notes", notesRoute);
+app.use("/auth", authRoute);
+app.use("*", (req, res) => {
+  res.send("404 not found");
+});
 
 export default app;
