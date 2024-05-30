70462f7448247d18a013aac8977394125f01c2fa
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-30 14:32:43 +0530

    remove middleware from register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { registerUser } from "../controllers/auth.js";
-import { filterOutEmptyProperties } from "../middlewares/filterOutEmptyProperties.js";
 
 const router = express.Router();
 
-router.post("/register", filterOutEmptyProperties, registerUser);
+router.post("/register", registerUser);
 router.post("/login", (req, res) => {
   res.send("login user");
 });
