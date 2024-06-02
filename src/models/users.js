bdcec35feaf10edeef7d0c3edeeec84d75d60687
Anish Pawaskar <anishpawaskar@gmail.com>
2024-06-02 20:32:23 +0530

    create model for fetching user by id from DB

--- src/models/users.js
+++ src/models/users.js
@@ -18,3 +18,21 @@ export const createNewUserModel = async (body) => {
 export const getUserByEmailModel = async (email) => {
   return await Users.findOne({ email });
 };
+
+export const getUserById = async (id) => {
+  try {
+    const user = await Users.findById(id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (err) {
+    if (err instanceof mongoose.CastError) {
+      throw new Error("Item not found");
+    }
+
+    throw err;
+  }
+};
