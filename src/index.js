851158406ca4499debdc0176374fcf2cb7c7a151
Anish Pawaskar <anishpawaskar@gmail.com>
2024-06-14 14:24:31 +0530

    add vercel config and default route for testing

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+
+app.get("/", (req, res) => {
+  return res.send("<h1>Hello World!</h1>");
+});
+
 app.use(indexRouter);
 
 app.use((err, req, res, next) => {
